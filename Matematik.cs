7699b7d6c1728994f33c168923ca32434eed0b5a
Enes Turan <153322224+Enes-Turan@users.noreply.github.com>
2025-10-25 00:08:22 +0300

    Add files via upload

---
+++ Matematik.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Metotlar_Uygulama1
+{
+	public class Matematik
+	{
+	    public decimal toplamaIslemi(decimal sayi1, decimal sayi2)
+		{
+			decimal sonuc = sayi1 + sayi2;
+			return sonuc;
+		}
+		public decimal cıkartmaIslemi(decimal sayi1,decimal sayi2)
+		{
+			decimal sonuc = sayi1 - sayi2;
+			return sonuc;
+		}
+		public decimal carpmaIslemi(decimal sayi1,decimal sayi2)
+		{
+			decimal sonuc = sayi1 * sayi2;
+			return sonuc;
+		}
+		public decimal bolmeIslemi(decimal sayi1 ,decimal sayi2)
+		{
+			decimal sonuc = sayi1 / sayi2;
+			return sonuc;
+		}
+		public void MenuHazırla() // Geriye bir şey dönmeyecek sadece ekrana bir şey yazdıracaksam void kullanılır.
+		{
+			Console.Clear();
+			Console.WriteLine("***** MENÜ *****");
+			Console.WriteLine("1-) Toplama işlemi ");
+            Console.WriteLine("2-) Çıkarma işlemi");
+            Console.WriteLine("3-) Çarpma işlemi");
+			Console.WriteLine("4-) Bölme işlemi");
+			Console.Write("Lütfen yapmak istediğiniz işlemi seçiniz : ");
+        }
+		public void sonucEkranaYaz(decimal ksayi1 ,decimal ksayi2 , decimal sonuc ,string operators)
+		{
+			Console.WriteLine("{0} {1} {2} = {3}",ksayi1,operators,ksayi2,sonuc);
+		}
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Metotlar_Uygulama1;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Matematik M = new Matematik();
+        YenidenIslemYap:
+        M.MenuHazırla();
+        int kullanicidanGelenDeger = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Lütfen 1.sayı değerini giriniz = ");
+        decimal ksayi1 = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Lütfen 2.sayı değerini giriniz = ");
+        decimal ksayi2 = decimal.Parse(Console.ReadLine());
+
+        decimal sonuc = 0;
+
+
+        switch (kullanicidanGelenDeger) { 
+            case 1:
+               sonuc = M.toplamaIslemi(ksayi1,ksayi2);
+                M.sonucEkranaYaz(ksayi1,ksayi2,sonuc,"+");
+                break;
+            case 2:
+                sonuc = M.cıkartmaIslemi(ksayi1,ksayi2);
+                M.sonucEkranaYaz(ksayi1,ksayi2,sonuc,"-");
+                break;
+            case 3:
+                sonuc = M.carpmaIslemi(ksayi1,ksayi2);
+                M.sonucEkranaYaz(ksayi1,ksayi2,sonuc,"*");
+                break;
+            case 4:
+                sonuc = M.bolmeIslemi(ksayi1,ksayi2);
+                M.sonucEkranaYaz(ksayi1,ksayi2,sonuc,"/");
+                break;
+            default:
+                Console.WriteLine("Geçersiz karakter girdiniz.");
+                Console.WriteLine("Lütfen işlem seçiniz = ");
+                System.Threading.Thread.Sleep(2000);
+                goto YenidenIslemYap;
+                break;
+        }
+        Console.WriteLine("İşlem yapmak istiyor musunuz ? ");
+        string EH = Console.ReadLine();
+        if (EH == "E" || EH == "e")
+        {
+            goto YenidenIslemYap;
+        }
+    }
+}
+
